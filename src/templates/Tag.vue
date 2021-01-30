04558e33f835fde61ce0d05541e0bdd38885568b
betteryangjie <betteryangjie@qq.com>
2021-01-30 23:32:38 +0800

    init

---
+++ src/templates/Tag.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>{{ $page.tag.title }}</h1>
+  </div>
+</template>
+
+<page-query>
+query ($id: ID!) {
+  tag: strapiTag (id: $id) {
+    id
+    title
+    posts {
+      id
+      title
+    }
+  }
+}
+</page-query>
+
+<script>
+export default {
+  name: 'TagPage'
+}
+</script>
+
+<style></style>
